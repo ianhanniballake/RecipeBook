6fddfffd3302531ff78a9615adbc231192f2c489
Ian Lake <ian.hannibal.lake@gmail.com>
2013-02-24 13:53:44 -0800

    Changes Ingredient and Instruction lists to use ArrayAdapter
    Will help when moving to edit mode
    
    Signed-off-by: Ian Lake <ian.hannibal.lake@gmail.com>

--- src/com/ianhanniballake/recipebook/ui/RecipeDetailIngredientFragment.java
+++ src/com/ianhanniballake/recipebook/ui/RecipeDetailIngredientFragment.java
@@ -1,20 +1,21 @@
 package com.ianhanniballake.recipebook.ui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.content.ContentUris;
-import android.content.Context;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
-import android.support.v4.widget.CursorAdapter;
-import android.support.v4.widget.ResourceCursorAdapter;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
 
 import com.ianhanniballake.recipebook.R;
 import com.ianhanniballake.recipebook.model.Ingredient;
@@ -26,47 +27,15 @@ import com.ianhanniballake.recipebook.provider.RecipeContract;
 public class RecipeDetailIngredientFragment extends ListFragment implements LoaderManager.LoaderCallbacks<Cursor>
 {
 	/**
-	 * Cursor Adapter which handles the unique binding of ingredients to a single TextView
-	 */
-	private class IngredientCursorAdapter extends ResourceCursorAdapter
-	{
-		/**
-		 * Standard constructor.
-		 * 
-		 * @param context
-		 *            The context where the ListView associated with this adapter is running
-		 * @param layout
-		 *            Resource identifier of a layout file that defines the views for this list item. Unless you
-		 *            override them later, this will define both the item views and the drop down views.
-		 * @param c
-		 *            The cursor from which to get the data.
-		 * @param flags
-		 *            Flags used to determine the behavior of the adapter, as per
-		 *            {@link CursorAdapter#CursorAdapter(Context, Cursor, int)}.
-		 */
-		public IngredientCursorAdapter(final Context context, final int layout, final Cursor c, final int flags)
-		{
-			super(context, layout, c, flags);
-		}
-
-		@Override
-		public void bindView(final View view, final Context context, final Cursor cursor)
-		{
-			final TextView raw = (TextView) view.findViewById(R.id.raw);
-			raw.setText(new Ingredient(cursor).toString());
-		}
-	}
-
-	/**
 	 * Adapter to display the list's data
 	 */
-	private IngredientCursorAdapter adapter;
+	private ArrayAdapter<Ingredient> adapter;
 
 	@Override
 	public void onActivityCreated(final Bundle savedInstanceState)
 	{
 		super.onActivityCreated(savedInstanceState);
-		adapter = new IngredientCursorAdapter(getActivity(), R.layout.list_item_ingredient, null, 0);
+		adapter = new ArrayAdapter<Ingredient>(getActivity(), R.layout.list_item_ingredient, R.id.raw);
 		setListAdapter(adapter);
 		getListView().setChoiceMode(AbsListView.CHOICE_MODE_NONE);
 		getLoaderManager().initLoader(0, null, this);
@@ -89,12 +58,21 @@ public class RecipeDetailIngredientFragment extends ListFragment implements Load
 	@Override
 	public void onLoaderReset(final Loader<Cursor> loader)
 	{
-		adapter.swapCursor(null);
+		Log.d(getClass().getSimpleName(), "onLoaderReset");
+		adapter.clear();
 	}
 
 	@Override
 	public void onLoadFinished(final Loader<Cursor> loader, final Cursor data)
 	{
-		adapter.swapCursor(data);
+		Log.d(getClass().getSimpleName(), "onLoadFinished: " + data.getCount());
+		adapter.setNotifyOnChange(false);
+		adapter.clear();
+		final List<Ingredient> newIngredients = new ArrayList<Ingredient>();
+		data.moveToPosition(-1);
+		while (data.moveToNext())
+			newIngredients.add(new Ingredient(data));
+		adapter.addAll(newIngredients);
+		adapter.notifyDataSetChanged();
 	}
 }
--- src/com/ianhanniballake/recipebook/ui/RecipeDetailInstructionFragment.java
+++ src/com/ianhanniballake/recipebook/ui/RecipeDetailInstructionFragment.java
@@ -1,20 +1,20 @@
 package com.ianhanniballake.recipebook.ui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.content.ContentUris;
-import android.content.Context;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
-import android.support.v4.widget.CursorAdapter;
-import android.support.v4.widget.ResourceCursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
 
 import com.ianhanniballake.recipebook.R;
 import com.ianhanniballake.recipebook.provider.RecipeContract;
@@ -25,50 +25,15 @@ import com.ianhanniballake.recipebook.provider.RecipeContract;
 public class RecipeDetailInstructionFragment extends ListFragment implements LoaderManager.LoaderCallbacks<Cursor>
 {
 	/**
-	 * Cursor Adapter which handles the binding of instructions to each list item
+	 * Adapter to display the list's data
 	 */
-	private class InstructionCursorAdapter extends ResourceCursorAdapter
-	{
-		/**
-		 * Standard constructor.
-		 * 
-		 * @param context
-		 *            The context where the ListView associated with this adapter is running
-		 * @param layout
-		 *            Resource identifier of a layout file that defines the views for this list item. Unless you
-		 *            override them later, this will define both the item views and the drop down views.
-		 * @param c
-		 *            The cursor from which to get the data.
-		 * @param flags
-		 *            Flags used to determine the behavior of the adapter, as per
-		 *            {@link CursorAdapter#CursorAdapter(Context, Cursor, int)}.
-		 */
-		public InstructionCursorAdapter(final Context context, final int layout, final Cursor c, final int flags)
-		{
-			super(context, layout, c, flags);
-		}
-
-		@Override
-		public void bindView(final View view, final Context context, final Cursor cursor)
-		{
-			final TextView instructionView = (TextView) view.findViewById(R.id.instruction);
-			final int instructionColumnIndex = cursor
-					.getColumnIndex(RecipeContract.Instructions.COLUMN_NAME_INSTRUCTION);
-			final String instruction = cursor.getString(instructionColumnIndex);
-			instructionView.setText(instruction);
-		}
-	}
-
-	/**
-	 * Adapter to display the list data
-	 */
-	private InstructionCursorAdapter adapter;
+	private ArrayAdapter<String> adapter;
 
 	@Override
 	public void onActivityCreated(final Bundle savedInstanceState)
 	{
 		super.onActivityCreated(savedInstanceState);
-		adapter = new InstructionCursorAdapter(getActivity(), R.layout.list_item_instruction, null, 0);
+		adapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_instruction, R.id.instruction);
 		setListAdapter(adapter);
 		getListView().setChoiceMode(AbsListView.CHOICE_MODE_NONE);
 		getLoaderManager().initLoader(0, null, this);
@@ -92,12 +57,21 @@ public class RecipeDetailInstructionFragment extends ListFragment implements Loa
 	@Override
 	public void onLoaderReset(final Loader<Cursor> loader)
 	{
-		adapter.swapCursor(null);
+		adapter.clear();
+		adapter.notifyDataSetInvalidated();
 	}
 
 	@Override
 	public void onLoadFinished(final Loader<Cursor> loader, final Cursor data)
 	{
-		adapter.swapCursor(data);
+		final int instructionColumnIndex = data.getColumnIndex(RecipeContract.Instructions.COLUMN_NAME_INSTRUCTION);
+		adapter.setNotifyOnChange(false);
+		adapter.clear();
+		final List<String> newInstructions = new ArrayList<String>();
+		data.moveToPosition(-1);
+		while (data.moveToNext())
+			newInstructions.add(data.getString(instructionColumnIndex));
+		adapter.addAll(newInstructions);
+		adapter.notifyDataSetChanged();
 	}
 }
